e0d4c0518b8c5adbfd5527edc732a04fe771fb72
Daniel Leon <dalobusiness@gmail.com>
2023-03-16 11:37:55 -0500

    Added copy button

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // Var Declaration
 const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const button = document.getElementById("random-button");
+const copyButton = document.getElementById("copy-button");
 const selectedColor = document.getElementById("selected-color");
 
 // Function Declaration
-
 function getRandomHex() {
   return Math.floor(Math.random() * hexValues.length);
 }
@@ -24,6 +24,11 @@ function changeColor() {
   selectedColor.style.color = color;
 }
 
+function copyBackground() {
+  navigator.clipboard.writeText(selectedColor.innerText);
+}
+
 //Event Listeners
 button.addEventListener("click", changeColor);
+copyButton.addEventListener("click", copyBackground);
 changeColor();
